cfbcff2f8b4563dc13eb31a398134b9ad92f7e0f
makestuff <github.com@m3.ath.cx>
2013-06-22 19:14:58 +0100

    Added fetch of common

--- msget.sh
+++ msget.sh
@@ -1,14 +1,16 @@
 #/bin/bash
 
+USER=makestuff
+BRANCH=master
+
 function usage {
 	echo "Synopsis: $0 [-b <branch>] [<meta>] <proj>" 1>&2
-	echo "  <branch> - the git branch to fetch (default: \"master\")" 1>&2
-	echo "  <user>   - the github.com user (default: \"makestuff\")" 1>&2
+	echo "  <branch> - the git branch to fetch (default: \"${BRANCH}\")" 1>&2
+	echo "  <user>   - the github.com user (default: \"${USER}\")" 1>&2
 	echo "  <repo>   - the github.com repository (required)" 1>&2
 	exit 1
 }
 
-BRANCH=master
 while [ "${1:0:1}" == "-" ]; do
 	OPT=${1:1}
 	case $OPT in
@@ -18,11 +20,7 @@ while [ "${1:0:1}" == "-" ]; do
 done
 
 if [ $# == 1 ]; then
-	USER=makestuff
 	REPO=$1
-elif [ $# == 2 ]; then
-	USER=$1
-	REPO=$2
 else
 	usage
 fi
@@ -32,6 +30,7 @@ if [ -e ${REPO} ]; then
 	exit 1
 fi
 
+echo "Fetching \"${USER}/${REPO}/${BRANCH}\"..."
 wget --no-check-certificate -q -O ${USER}-${REPO}-${BRANCH}.tgz https://github.com/${USER}/${REPO}/archive/${BRANCH}.tar.gz
 if [ "$?" != 0 ]; then
 	echo "Fetch of \"${USER}/${REPO}/${BRANCH}\" failed. Are you sure it exists?" 1>&2
@@ -39,9 +38,37 @@ if [ "$?" != 0 ]; then
 	exit 1
 fi
 
+echo "Uncompressing \"${USER}/${REPO}/${BRANCH}\" into \"${REPO}\" directory..."
 tar zxf ${USER}-${REPO}-${BRANCH}.tgz
 mv ${REPO}-${BRANCH} ${REPO}
 echo ${BRANCH} > ${REPO}/.branch
 rm -f ${USER}-${REPO}-${BRANCH}.tgz
 
-echo "Successfully unpacked \"${USER}/${REPO}/${BRANCH}\" into \"${REPO}\". Have fun!"
+TOPDIR=$(dirname $(dirname $0))
+if [ -e ${TOPDIR}/common ]; then
+	if [ -e ${TOPDIR}/common/.branch ]; then
+		COMMON_BRANCH=$(cat ${TOPDIR}/common/.branch)
+	else
+		COMMON_BRANCH=master
+	fi
+	if [ "${COMMON_BRANCH}" != "${BRANCH}" ]; then
+		echo
+		echo "ERROR: A \"${TOPDIR}/common\" directory exists, but it's on the ${COMMON_BRANCH} branch. You" 2>&1
+		echo "       should not try to mix different versions together; they must be" 2>&1
+		echo "       consistent!" 2>&1
+		exit 1
+	fi
+else
+	echo "Fetching \"${USER}/common/${BRANCH}\"..."
+	wget --no-check-certificate -q -O ${USER}-common-${BRANCH}.tgz https://github.com/${USER}/common/archive/${BRANCH}.tar.gz
+	if [ "$?" != 0 ]; then
+		echo "Fetch of \"${USER}/common/${BRANCH}\" failed. Are you sure it exists?" 1>&2
+		rm -f ${USER}-common-${BRANCH}.tgz
+		exit 1
+	fi
+	echo "Uncompressing \"${USER}/common/${BRANCH}\" into \"${TOPDIR}/common\" directory..."
+	tar zxf ${USER}-common-${BRANCH}.tgz
+	echo ${BRANCH} > common-${BRANCH}/.branch
+	mv common-${BRANCH} ${TOPDIR}/common
+	rm -f ${USER}-common-${BRANCH}.tgz
+fi
